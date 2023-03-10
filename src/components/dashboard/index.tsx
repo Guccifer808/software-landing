b17e78ade317a37fc27177bf49ddcb663af4b448
G808 <hucciallstars@gmail.com>
2023-03-10 19:46:08 +0200

    Testing 2

---
+++ src/components/dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+
+type Props = {};
+
+const Dashboard = (props: Props) => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Dashboard;
