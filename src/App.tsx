822d9dacbb7987dffb3ed08b1e8ac5bff4578d37
G808 <hucciallstars@gmail.com>
2023-03-10 19:37:07 +0200

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+
 import Homepage from "./components/Homepage";
 import Dashboard from "./components/Dashboard";
 import PrivateRoute from "./components/PrivateRoute";
@@ -26,7 +27,6 @@ type Props = {
   // path: string;
   // element: React.ReactNode;
 };
-// redirectUri: window.location.origin,
 
 //software-landing-one.vercel.app/dashboard?code=wq4J1c8F2H81jP3DJiXvs7DIC49tmAljY9oWEzktfjVpT&state=LVlQLkowTW0tUkNHRG5IM29pdV96THFVTm9mUEVENTRHb1dyUHJLNHloMw%3D%3D
 
@@ -37,8 +37,11 @@ const auth0Options = {
     redirect_uri: window.location.origin + "/dashboard",
   },
 };
-const navigate = useNavigate();
-const onRedirectCallback = (appState: any, navigate: any) => {
+const onRedirectCallback = (
+  appState: { returnTo?: string } | undefined
+): void => {
+  const navigate = useNavigate();
+
   // If the user is returning to a specific page, navigate to that page
   if (appState && appState.returnTo) {
     return navigate(appState.returnTo);
@@ -47,6 +50,7 @@ const onRedirectCallback = (appState: any, navigate: any) => {
   // Otherwise, navigate to the dashboard
   return navigate("/dashboard");
 };
+
 function App({ setSelectedPage }: Props) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const { isLoading, isAuthenticated: auth0IsAuthenticated } = useAuth0();
@@ -59,10 +63,7 @@ function App({ setSelectedPage }: Props) {
     setIsAuthenticated(true);
   }
   return (
-    <Auth0Provider
-      {...auth0Options}
-      onRedirectCallback={(appState) => onRedirectCallback(appState, navigate)}
-    >
+    <Auth0Provider {...auth0Options} onRedirectCallback={onRedirectCallback}>
       <Router>
         <div className="app">
           <Routes>
