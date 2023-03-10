d39b036ba859db96464cc662c295cc3cd331d902
G808 <hucciallstars@gmail.com>
2023-03-10 19:11:29 +0200

    Testing

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -4,17 +4,17 @@ import { Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 
 type Props = {
-  path: string;
+  //   path: string;
 };
 
-const PrivateRoute: FC<Props> = ({ path, ...rest }: Props) => {
+const PrivateRoute: FC<Props> = ({}: Props) => {
   const { isAuthenticated } = useAuth0();
 
   if (isAuthenticated) {
-    return <Route path={path} element={<Dashboard />} />;
+    return <Route path="/dashboard" element={<Dashboard />} />;
   } else {
     return (
-      <Route path={path} element={<Navigate to={{ pathname: "/login" }} />} />
+      <Route path="/login" element={<Navigate to={{ pathname: "/login" }} />} />
     );
   }
 };
