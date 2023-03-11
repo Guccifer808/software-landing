13f201a45a619e00280d41751065b4c902e3f54d
G808 <hucciallstars@gmail.com>
2023-03-11 23:51:18 +0200

    Fixing navigation after callback Auth0

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { SelectedPage } from "./shared/types";
+// import { SelectedPage } from "./shared/types";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App setSelectedPage={(value: SelectedPage) => {}} />
+    <App />
   </React.StrictMode>
 );
