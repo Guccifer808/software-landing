61afc00c520f7ef95dbb35ffa0487deb95d77f3d
G808 <hucciallstars@gmail.com>
2023-03-12 00:23:24 +0200

    Fixing navigation after callback Auth0

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,28 @@
+import { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 type Props = {};
 
 const Login = (props: Props) => {
   const { loginWithRedirect } = useAuth0();
   const location = useLocation();
+  const { isAuthenticated, getIdTokenClaims } = useAuth0();
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    const handleRedirectCallback = async () => {
+      // If the user is not authenticated, redirect to the homepage
+      if (!isAuthenticated) {
+        navigate("/");
+        return;
+      }
+      navigate("/dashboard");
+    };
+
+    // Call the handleRedirectCallback function when the component mounts
+    handleRedirectCallback();
+  }, [isAuthenticated, getIdTokenClaims, navigate]);
   // Save the current path so we can redirect back to it after login
   const returnTo = location.pathname;
 
