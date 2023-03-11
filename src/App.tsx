d97daef2d6899b3a26723df9178cd6160789989a
G808 <hucciallstars@gmail.com>
2023-03-12 00:17:43 +0200

    Fixing navigation after callback Auth0

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,24 @@ import {
   Routes,
   Route,
   useNavigate,
+  NavigateFunction,
 } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
 import Dashboard from "./components/dashboard";
 import Login from "./pages/Login";
 
-function onRedirectCallback(appState: { returnTo?: string } | undefined): void {
-  const navigate = useNavigate();
-
-  // If the user is returning to a specific page, navigate to that page
-  // if (appState && appState.returnTo) {
-  //   navigate(appState.returnTo);
-  // }
-
-  // Otherwise, navigate to the dashboard
-  navigate("/dashboard");
+function onRedirectCallback(
+  appState: { returnTo?: string } | undefined,
+  navigate: NavigateFunction
+): void {
+  if (appState && appState.returnTo) {
+    navigate(appState.returnTo);
+  } else {
+    navigate("/dashboard");
+  }
 }
+
 const auth0Options = {
   domain: "dev-3wfcqahpvqzaetgv.us.auth0.com",
   clientId: "FrRLilRP9KUMA8yfwKMTlpy1e82Ic97D",
@@ -30,14 +31,18 @@ const auth0Options = {
 };
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <Auth0Provider {...auth0Options} onRedirectCallback={onRedirectCallback}>
+    <Auth0Provider
+      {...auth0Options}
+      onRedirectCallback={(appState) => onRedirectCallback(appState, navigate)}
+    >
       <Router>
         <div className="app">
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/login" element={<Login />} />
-            {/* <Route path="/callback" element={<Dashboard />} /> */}
+            <Route path="/callback" element={<Dashboard />} />
             <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </div>
